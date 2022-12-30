2c2a29a933dcb27877951a5f396d9aee3e81c741
LDtito <henry.jl.live@gmail.com>
2022-12-30 15:12:54 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { NavLeftComponent } from './shared/nav-left/nav-left.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProductComponent,
+    CategoryComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    NavLeftComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
